67820d8001e68fa7d20d883bf9c11fdf37d372da
Khoi <nvkhoi@IfiSolution.local>
2024-07-01 16:10:45 +0700

    Server app

--- src/main/java/com/example/client/AppConfig.java
+++ src/main/java/com/example/client/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.client;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 	@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+		}
+		
 }
